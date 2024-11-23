47e1f2f2b8193c31b3d729ba13af7ccef4182e8f
Hamzah Fayad <hamzahfayad99@gmail.com>
2024-11-23 15:13:26 +0100

    add leaflet map

--- my-svelte-app/src/routes/map/+page.svelte
+++ my-svelte-app/src/routes/map/+page.svelte
@@ -1,7 +1,32 @@
-<h2>Map</h2>
+<script>
+  // @ts-ignore
+  import { onMount } from "svelte";
+
+  let map;
+
+  onMount(() => {
+    // Initialize the map
+    // @ts-ignore
+    map = L.map("map").setView([52.52, 13.405], 10); // Default coordinates
+
+    // @ts-ignore
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Add a marker
+    // @ts-ignore
+    L.marker([52.52, 13.405]).addTo(map).bindPopup("Lorem Ipsum").openPopup();
+  });
+</script>
+
+<div id="map"></div>
 
 <style>
-  h2 {
-    text-align: center;
+  #map {
+    height: 75vh;
+    margin: 0 auto;
+    width: 100%;
   }
 </style>
